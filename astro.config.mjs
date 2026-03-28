fa4ff19785a3549bff6f495cc153c815a8d37646
Roberdev <r.romerod96@gmail.com>
2026-03-28 10:14:12 -0400

    PokeApi-Astro2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
-import netlify from '@astrojs/netlify'; 
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://RoberDev96.github.io',
-  base: '/PokeApi-Astro',
+  output: 'server', 
+  adapter: netlify(),
   integrations: [icon(), solidJs()],
 });
