6301635c581d352c7160a367dc38c2fc09283f5f
Roberdev <r.romerod96@gmail.com>
2026-03-28 10:03:50 -0400

    PokeApi-Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
+import netlify from '@astrojs/netlify'; 
 
 // https://astro.build/config
 export default defineConfig({
