a9ace22e02785a5391fc560a94957e115d49e0e1
Roberdev <r.romerod96@gmail.com>
2026-04-14 16:12:12 -0400

    poke astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
-import netlify from '@astrojs/netlify';
+import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server', 
-  adapter: netlify(),
+  
   integrations: [icon(), solidJs()],
+  adapter: vercel(),  
 });
